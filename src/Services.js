523e8d067c5a66769eae8632ae9ee79f285c4a21
Rilo <taydh.wade@gmail.com>
2023-11-07 12:37:05 -0800

    removed aura references from boxes

--- src/Services.js
+++ src/Services.js
@@ -22,15 +22,15 @@ const Services = () => {
             <div onClick={artServicesClickHandler}>{artServicesModal && <ArtServices/>}</div>
             <div onClick={eventServicesClickHandler}>{eventServicesModal && <EventServices/>}</div>
             <div className="art-services-container box" onClick={artServicesClickHandler}>
-               <p style={{fontSize:"20px"}}>Art Services</p>
+               <p style={{fontSize:"20px"}}>Balloons</p>
                <br/>
-               <p style={{fontSize:"30px"}}>Rainbow Aura Emporia</p> 
+               <p style={{fontSize:"30px"}}></p> 
                 
             </div>
             <div className="event-services-container box" onClick={eventServicesClickHandler}>
-              <p style={{fontSize:"20px"}}>Event Services</p> 
+              <p style={{fontSize:"20px"}}>Fire/LED</p> 
               <br/>  
-              <p style={{fontSize:"30px"}}>Rainbow Aura Party</p>
+              <p style={{fontSize:"30px"}}></p>
             </div>
         </div>
     )
