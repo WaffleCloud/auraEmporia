4ebe54682227fe59b6f5b8ca0004a24b5963b6d2
Rilo <taydh.wade@gmail.com>
2023-11-07 17:23:54 -0800

    trying to add insta feed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import InstaFeeds from './InstaFeeds';
 import IntroContainer from './IntroContainer';
 import Gallery from './Gallery';
 import Contact from './Contact';
@@ -10,6 +11,7 @@ function App() {
     <div className="container">
       <Gallery/>
       <IntroContainer/>
+      <InstaFeeds token={process.env.TOKEN} limit={12}/>
       <Contact/>
       <Services/>
     </div>
--- src/Services.js
+++ src/Services.js
@@ -21,13 +21,13 @@ const Services = () => {
         <div className="services-container">
             <div onClick={artServicesClickHandler}>{artServicesModal && <ArtServices/>}</div>
             <div onClick={eventServicesClickHandler}>{eventServicesModal && <EventServices/>}</div>
-            <div className="art-services-container box" onClick={artServicesClickHandler}>
+            <div className="art-services-container small-box" onClick={artServicesClickHandler}>
                <p style={{fontSize:"20px"}}>Balloons</p>
                <br/>
                <p style={{fontSize:"30px"}}></p> 
                 
             </div>
-            <div className="event-services-container box" onClick={eventServicesClickHandler}>
+            <div className="event-services-container small-box" onClick={eventServicesClickHandler}>
               <p style={{fontSize:"20px"}}>Fire/LED</p> 
               <br/>  
               <p style={{fontSize:"30px"}}></p>
